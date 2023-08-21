bf9dbe2abee0b09864defae0f3fb753d0149f1f2
Ebru Karayel <ebru.karayel@ckenerji.com>
2023-08-21 11:51:44 +0300

    login tc değişiklikleri1

---
+++ Scripts/Verify the Login of Site-NS3/Script1692605801269.groovy
@@ -0,0 +1,31 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.urlLoginPage)
+
+WebUI.verifyElementPresent(findTestObject('Page_OrangeHRM/inp_Username'), 5)
+
+WebUI.verifyElementVisible(findTestObject('Page_OrangeHRM/inp_Username'))
+
+WebUI.verifyElementPresent(findTestObject('Page_OrangeHRM/inp_Password'), 5)
+
+WebUI.verifyElementVisible(findTestObject('Page_OrangeHRM/inp_Password'))
+
+WebUI.verifyElementPresent(findTestObject('Page_OrangeHRM/btn_Login'), 5)
+
+WebUI.verifyElementVisible(findTestObject('Page_OrangeHRM/btn_Login'))
+
+WebUI.verifyElementClickable(findTestObject('Page_OrangeHRM/btn_Login'))
+
+'invalid senario 3: invalid username, invalid password.'
+WebUI.setText(findTestObject('Object Repository/Page_OrangeHRM/inp_Username'), GlobalVariable.username_invalid)
+
+WebUI.setText(findTestObject('Page_OrangeHRM/inp_Password'), GlobalVariable.password_invalid)
+
+WebUI.click(findTestObject('Object Repository/Page_OrangeHRM/btn_Login'))
+
+WebUI.closeBrowser()
+
